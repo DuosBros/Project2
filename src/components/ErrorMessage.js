e743c495ddbf04657e2c1e6f10028382581c8725
An Tran <atran1@gvcgroup.com>
2019-02-19 18:54:45 +0100

    ErrorMessage: ternary operator => var

--- src/components/ErrorMessage.js
+++ src/components/ErrorMessage.js
@@ -5,35 +5,33 @@ import pikachu from '../assets/pikachu.jpg'
 const ErrorMessage = (props) => {
     const [isRefreshButtonLoading, toggleRefreshButtonLoading] = useState(0);
 
-    handleRefresh = () => {
+    const handleRefresh = () => {
         toggleRefreshButtonLoading(true);
         props.handleRefresh();
         toggleRefreshButtonLoading(false);
     }
 
+    let refresh = null;
+    if (props.handleRefresh) {
+        refresh = (
+            <>
+                Try again
+                <Icon
+                    className="pointerCursor"
+                    onClick={() => handleRefresh()}
+                    name="refresh"
+                    loading={isRefreshButtonLoading} />
+            </>
+        )
+    }
+    
     return (
         <Message icon>
             <Image src={pikachu} size='tiny' spaced />
             <Message.Content>
                 <Message.Header>{(props.title || "Ooops something went wrong")}</Message.Header>
                 {props.message === "" ? null : <>{props.message} <br /></>}
-
-                {
-                    props.handleRefresh ?
-                        (
-                            <>
-                                Try again
-                                    <Icon
-                                    className="pointerCursor"
-                                    onClick={() => this.handleRefresh()}
-                                    name="refresh"
-                                    loading={isRefreshButtonLoading} />
-                            </>
-                        ) : (
-                            null
-                        )
-                }
-
+                {refresh}
             </Message.Content>
         </Message>
     )
