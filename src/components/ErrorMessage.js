b94399dfe1edec5207bda39b47019e44a7bda34f
An Tran <atran1@gvcgroup.com>
2019-02-20 09:35:14 +0100

    ErrorMessage: Added accordion for err stack

--- src/components/ErrorMessage.js
+++ src/components/ErrorMessage.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
-import { Message, Icon, Image } from 'semantic-ui-react'
+import { Message, Icon, Image, Accordion } from 'semantic-ui-react'
 import pikachu from '../assets/pikachu.jpg'
 
 const ErrorMessage = (props) => {
-    const [isRefreshButtonLoading, toggleRefreshButtonLoading] = useState(0);
+    const [isRefreshButtonLoading, toggleRefreshButtonLoading] = useState(false);
+    const [isAccordionExpanded, toggleAccordion] = useState(false);
 
     const handleRefresh = () => {
         toggleRefreshButtonLoading(true);
@@ -24,7 +25,28 @@ const ErrorMessage = (props) => {
             </>
         )
     }
-    
+
+    const handleToggleAccordion = () => {
+        toggleAccordion(!isAccordionExpanded)
+    }
+
+    let errorAccordion = null;
+    if (props.error) {
+        errorAccordion = (
+            <Accordion>
+                <Accordion.Title active={isAccordionExpanded} index={0} onClick={() => handleToggleAccordion()}>
+                    <Icon name='dropdown' />
+                    Details
+                        </Accordion.Title>
+                <Accordion.Content active={isAccordionExpanded}>
+                    {props.error && props.error.toString()}
+                    <br />
+                    {props.error.stack}
+                </Accordion.Content>
+            </Accordion>
+        )
+    }
+
     return (
         <Message icon>
             <Image src={pikachu} size='tiny' spaced />
@@ -32,6 +54,7 @@ const ErrorMessage = (props) => {
                 <Message.Header>{(props.title || "Ooops something went wrong")}</Message.Header>
                 {props.message === "" ? null : <>{props.message} <br /></>}
                 {refresh}
+                {errorAccordion}
             </Message.Content>
         </Message>
     )
