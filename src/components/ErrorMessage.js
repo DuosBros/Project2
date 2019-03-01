7539160d670341351bf2dcec95d09eaeeb41bf9e
An Tran <atran1@gvcgroup.com>
2019-03-01 12:44:08 +0100

    ErrorMessage: do not show stack when its prod

--- src/components/ErrorMessage.js
+++ src/components/ErrorMessage.js
@@ -13,7 +13,7 @@ export default class ErrorMessage extends Component {
 
     componentDidMount() {
         const { error, errorInfo } = this.props;
-        if(error || errorInfo) {
+        if (error || errorInfo) {
             console.log("An error occurred. Here are some details:", { error, errorInfo });
         }
     }
@@ -32,14 +32,14 @@ export default class ErrorMessage extends Component {
         const { isAccordionExpanded } = this.state;
         let errorDetails = [];
         if (error) {
-            if(error.hasOwnProperty("name")) {
+            if (error.hasOwnProperty("name")) {
                 errorDetails.push((
                     <div key="name">
                         <strong>{error.name}</strong>
                     </div>
                 ));
             }
-            if(error.hasOwnProperty("message")) {
+            if (error.hasOwnProperty("message")) {
                 errorDetails.push((
                     <div key="message">
                         <pre style={{ whiteSpace: "pre-wrap" }}>{error.message.split('\n')[0]}</pre>
@@ -48,7 +48,7 @@ export default class ErrorMessage extends Component {
             }
         }
 
-        if(errorInfo && errorInfo.hasOwnProperty("componentStack")) {
+        if (errorInfo && errorInfo.hasOwnProperty("componentStack")) {
             errorDetails.push((
                 <div key="stack" style={{ overflow: "auto" }}>
                     <pre>{errorInfo.componentStack}</pre>
@@ -56,7 +56,7 @@ export default class ErrorMessage extends Component {
             ));
         }
 
-        if(errorDetails.length < 1) {
+        if (errorDetails.length < 1) {
             return null;
         }
 
@@ -88,14 +88,14 @@ export default class ErrorMessage extends Component {
                 <div>
                     Try again
                     <Icon
-                    className="pointerCursor"
-                    onClick={this.handleRefresh}
-                    name="refresh"/>
+                        className="pointerCursor"
+                        onClick={this.handleRefresh}
+                        name="refresh" />
                 </div>
             )
         }
 
-        let errorAccordion = this.renderErrorAccordeon(error, errorInfo);
+        let errorAccordion = process.env.NODE_ENV === 'development' ? this.renderErrorAccordeon(error, errorInfo) : null;
 
         return (
             <Message icon>
