72de84e5802c2601433147dc4d5c6c1d4e1a363c
Ferdinand Holzer <ferdinand.holzer@gmail.com>
2019-03-03 15:45:41 +0100

    fix obscure horizontal scrolling issue
    * width: 100% on footer was causing it to overflow page to the
      right by 200px (sidebar with)
    * remove unneeded grid as it has negative margin, again
      overflowing to the right
    
    These overflows cause issues with drag and drop libraries.

--- src/utils/Footer.js
+++ src/utils/Footer.js
@@ -8,16 +8,9 @@ export default class Footer extends React.Component {
         return (
             <div {...this.props}>
                 <Divider />
-                <Grid>
-                    <Grid.Row columns='equal'>
-
-                        <Grid.Column>
-                            © {(new Date()).getFullYear()} <a href="mailto:d.leanops.sports.b2c@gvcgroup.com">VIE LeanOps B2C</a> | 
-                             Issue? <a href="https://vie.git.bwinparty.com/groups/leanops/Loco/-/issues" target="_blank" rel="noopener noreferrer">Report it!</a> <Icon name="bug" ></Icon>
-                             | Version: {packageJson.version}
-                        </Grid.Column>
-                    </Grid.Row>
-                </Grid>
+                © {(new Date()).getFullYear()} <a href="mailto:d.leanops.sports.b2c@gvcgroup.com">VIE LeanOps B2C</a> |
+                 Issue? <a href="https://vie.git.bwinparty.com/groups/leanops/Loco/-/issues" target="_blank" rel="noopener noreferrer">Report it!</a> <Icon name="bug" ></Icon>
+                 | Version: {packageJson.version}
             </div>
         )
     }
