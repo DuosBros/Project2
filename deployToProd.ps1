f196f4bb13166a6a26e792ae36447a204e238426
An Tran <atran1@gvcgroup.com>
2019-03-03 13:27:09 +0100

    adding date in deploy script

--- deployToProd.ps1
+++ deployToProd.ps1
@@ -1,3 +1,4 @@
-echo "Copying items..."
-copy-item -Recurse -Path "./build/*" -Destination "\\Atvp1wwlco101\d$\WWWroot\locov2.prod.env.works" -Force
-echo "Deployed!"
+Write-Host "Started at [" (Get-Date -Format "HH:mm:ss dd.MM.yyyy") "]" -ForegroundColor Yellow
+Write-Host "Copying items..." 
+Copy-Item -Recurse -Path "./build/*" -Destination "\\Atvp1wwlco101\d$\WWWroot\locov2.prod.env.works" -Force
+Write-Host "Deployed!" -ForegroundColor Green
