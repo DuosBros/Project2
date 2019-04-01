09e182360eaa89ac5c94534edf91f4dbadfd4a89
An Tran <atran1@gvcgroup.com>
2019-04-01 15:32:52 +0200

    forgot the file

--- src/requests/MiscAxios.js
+++ src/requests/MiscAxios.js
@@ -5,6 +5,6 @@ export function getServiceVirtualMachines() {
     return axios.get(LOCO_API + 'servicevirtualmachine');
 }
 
-export function exportDataToExcel(data, fileName, sheetName) {
-    return axios.post(PM_API + 'export/' + fileName + '/' + sheetName, data, { responseType: 'blob' })
+export function exportDataToExcel(data, fileName) {
+    return axios.post(PM_API + 'export/' + fileName, data, { responseType: 'blob' })
 }
