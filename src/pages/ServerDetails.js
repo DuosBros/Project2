27979b346ae6cd79b3fdf7100ddb5530497bd5a7
fholzer <fholzer@gvcgroup.com>
2019-04-08 00:09:41 +0200

    Change date output for deploymentStats on ServerDetails and ServiceDetails page

--- src/pages/ServerDetails.js
+++ src/pages/ServerDetails.js
@@ -314,7 +314,7 @@ class ServerDetails extends React.Component {
                     return (
                         <List.Item key={i}>
                             <List.Header>
-                                {moment(x.deployDateTime).local().format("DD.MM.YYYY HH:mm")} | {x.userName} <Popup closeOnPortalMouseLeave={true} trigger={<Icon size='small' name='question' />}><Popup.Content><pre>{JSON.stringify(x, null, 2)}</pre></Popup.Content></Popup>
+                                {moment(x.deployDateTime).local().fromNow()} | {x.userName} <Popup closeOnPortalMouseLeave={true} trigger={<Icon size='small' name='question' />}><Popup.Content><pre>{JSON.stringify(x, null, 2)}</pre></Popup.Content></Popup>
                             </List.Header>
                             Version: {x.version} {x.changeNumber && " | CHG: " + x.changeNumber}
                         </List.Item>
