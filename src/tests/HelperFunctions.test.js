a6aaa72a66e8478b22108706babe9cd239170bd8
fholzer <fholzer@gvcgroup.com>
2019-04-07 18:04:53 +0200

    Revert "introduced jest tests"
    This reverts commit 70387e3a2e70ffa0948552fe723a74fed2fe2855.

--- src/tests/HelperFunctions.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { isNum } from '../utils/HelperFunction';
-
-it('isNum', () => {
-    expect(isNum("5")).toEqual(true)
-});
--- src/tests/index.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AppRoutes from '../utils/appRoutes';
-
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<AppRoutes />, div);
-});
