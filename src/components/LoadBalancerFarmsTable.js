0705fcac2cdef29ac72f4b2af3fa74ac88b4fa17
An Tran <atran1@gvcgroup.com>
2019-04-24 14:05:04 +0200

    lbfarm table inline details fix - add one more property

--- src/components/LoadBalancerFarmsTable.js
+++ src/components/LoadBalancerFarmsTable.js
@@ -227,7 +227,7 @@ class LoadBalancerFarmsTable extends React.PureComponent {
     }
 
     getDataKey(data) {
-        return data.IpAddress + "-" + data.Name + "-" + data.VsStatusLastUpdate;
+        return data.IpAddress + "-" + data.Name + "-" + data.VsStatusLastUpdate + "-" + data.Pool;
     }
 
 
