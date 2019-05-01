23c2c544298953112a8a13ad89f21674aff0743f
An Tran <atran1@gvcgroup.com>
2019-05-01 17:19:34 +0200

    add id when composing a key to have it bullet proof

--- src/components/LoadBalancerFarmsTable.js
+++ src/components/LoadBalancerFarmsTable.js
@@ -227,7 +227,7 @@ class LoadBalancerFarmsTable extends React.PureComponent {
     }
 
     getDataKey(data) {
-        return data.IpAddress + "-" + data.Name + "-" + data.VsStatusLastUpdate + "-" + data.Pool + "-" + data.Label;
+        return data.Id + "-" + data.IpAddress + "-" + data.Name + "-" + data.VsStatusLastUpdate + "-" + data.Pool + "-" + data.Label;
     }
 
 
